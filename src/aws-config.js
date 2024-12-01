dc2e91aac4c0441c8ae096ea7938248de6708e84
wpeladmaster <drore@elad.co.il>
2024-12-01 19:38:51 +0200

    Change cognito keys

--- src/aws-config.js
+++ src/aws-config.js
@@ -2,9 +2,9 @@ import Amplify from 'aws-amplify';
 
 Amplify.configure({
   Auth: {
-    region: 'us-east-1_yFeAbcXhB',
-    userPoolId: 'us-east-1_yFeAbcXhB',
-    userPoolWebClientId: '47oeprabl0kv6j1rftoggkcmdu',
+    region: 'us-east-1_zXmU8v3CU',
+    userPoolId: 'us-east-1_zXmU8v3CU',
+    userPoolWebClientId: '6mh9igv6qb8mjttb5ploesjfe',
   },
   Storage: {
     AWSS3: {
