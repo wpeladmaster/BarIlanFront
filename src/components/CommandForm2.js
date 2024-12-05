abb3e8608b8bd43ba3b4cf1bf42f5c3d5e28b861
wpeladmaster <drore@elad.co.il>
2024-12-05 19:10:59 +0200

    Add port 3002

--- src/components/CommandForm2.js
+++ src/components/CommandForm2.js
@@ -10,7 +10,7 @@ const CommandForm2 = () => {
         throw new Error('Command cannot be empty');
       }
 
-      const response = await fetch('http://localhost:3000/run-command', {
+      const response = await fetch('http://localhost:3002/run-command', {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json'
