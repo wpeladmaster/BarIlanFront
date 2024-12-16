574f50ef78ceb69a31864dac5a021270d2cf859a
Dror Efraim <139956959+wpeladmaster@users.noreply.github.com>
2024-12-16 09:48:02 +0200

    Remove saml init

--- src/App.js
+++ src/App.js
@@ -8,50 +8,22 @@ import Footer from './components/Footer';
 import Login from './components/Login';
 import HomePage from './components/HomePage';
 import AdminSearch from './components/AdminSearch';
-import { PublicClientApplication } from '@azure/msal-browser';
-import fetchGroupNames from './utils/fetchGroupNames'; // Import the updated fetchGroupNames
+import fetchGroupNames from './utils/fetchGroupNames';
 import { loginRequest } from './authConfig';
 
 const App = () => {
-  const { accounts } = useMsal();
+  const { instance: msalInstance, accounts } = useMsal();
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [isLoading, setIsLoading] = useState(true);
   const [userName, setUserName] = useState('');
   const [userRole, setUserRole] = useState([]);
   const [accessToken, setAccessToken] = useState('');
-  const [groupNames, setGroupNames] = useState([]); // State to store fetched group names
-  const [msalInstance, setMsalInstance] = useState(null);
-
-  const msalConfig = {
-    auth: {
-      clientId: 'aadb3f2f-d35f-4080-bc72-2ee32b741120',
-      authority: 'https://login.microsoftonline.com/352ed1fa-2f18-487f-a4cf-4804faa235c7/saml2',
-      redirectUri: 'https://main.d3u5rxv1b6pn2o.amplifyapp.com/homepage',
-    },
-  };
-
-  useEffect(() => {
-    const initializeMsal = async () => {
-      try {
-        const newMsalInstance = new PublicClientApplication(msalConfig);
-        setMsalInstance(newMsalInstance);
-      } catch (error) {
-        console.error('Error initializing MSAL:', error);
-      }
-    };
-
-    initializeMsal();
-  }, []);
+  const [groupNames, setGroupNames] = useState([]);
 
   useEffect(() => {
     const checkSession = async () => {
       try {
         console.log('App.js: Checking user session...');
-        if (!msalInstance) {
-          console.error('App.js: MSAL instance is not initialized.');
-          return;
-        }
-
         const allAccounts = msalInstance.getAllAccounts();
         if (!allAccounts.length) {
           console.log('App.js: No active accounts found.');
@@ -60,13 +32,14 @@ const App = () => {
         }
 
         const account = msalInstance.getActiveAccount() || allAccounts[0];
+        msalInstance.setActiveAccount(account); // Set the active account
         if (account) {
           console.log('App.js: Account found:', account);
           setIsAuthenticated(true);
           setUserName(account.idTokenClaims.name);
 
           const tokenResponse = await msalInstance.acquireTokenSilent({
-            scopes: ['user.read'],
+            ...loginRequest,
             account,
           });
 
@@ -82,8 +55,6 @@ const App = () => {
           const groupNames = await fetchGroupNames(roles, tokenResponse.accessToken);
           console.log('App.js: Group names fetched:', groupNames);
           setGroupNames(groupNames);
-        } else {
-          console.log('App.js: No active account.');
         }
       } catch (error) {
         console.error('App.js: Error during session check:', error);
@@ -92,23 +63,18 @@ const App = () => {
       }
     };
 
-    // Wait until the MSAL instance is set before checking the session
-    if (msalInstance) {
-      checkSession();
-    }
+    checkSession();
   }, [msalInstance]);
 
   const handleLogout = async () => {
     try {
       console.log('App.js: Logging out...');
-      if (msalInstance) {
-        await msalInstance.logoutPopup();
-      }
+      await msalInstance.logoutPopup();
       setIsAuthenticated(false);
       setUserName('');
       setUserRole([]);
       setAccessToken('');
-      setGroupNames([]); // Clear group names on logout
+      setGroupNames([]);
       console.log('App.js: Logout successful, state cleared.');
     } catch (error) {
       console.error('App.js: Logout error:', error);
