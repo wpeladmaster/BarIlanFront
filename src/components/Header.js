abf4a9295247e1a8fed857115e91c7e7f0ad1f39
wpeladmaster <drore@elad.co.il>
2024-12-17 13:19:02 +0200

    Add header design

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import '../style/Header.scss'; 
 
 const Header = ({ isAuthenticated, onLogout, onLogin, userName }) => {
   return (
     <header>
-      <nav>
-        <div>
-          <h1>Welcome, {isAuthenticated ? userName : 'Guest'}</h1>
-        </div>
-        <div>
-          {isAuthenticated ? (
+      <div className="auth-wrap">
+        {isAuthenticated ? (
+          <div className='inner'>
             <button onClick={onLogout}>Logout</button>
-          ) : (
+            <span>Welcome, {isAuthenticated ? userName : 'Guest'}</span>
+          </div>
+        ) : (
+          <div className='inner'>
             <button onClick={onLogin}>Login</button>
-          )}
-        </div>
+          </div>
+        )}
+      </div>
+      <div className='logo'>
+        <img src='../../images/bar-ilan-logo.png' alt='Logo' />
+      </div>
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          {/* {userRole && userRole.includes('Admins') && (
+            <li><Link to="/admin-search">Admin Search</Link></li>
+          )} */}
+        </ul>
       </nav>
     </header>
   );
