f60898d9b3bdcf85054e3bc576c28a5f8f0eef17
wpeladmaster <drore@elad.co.il>
2024-12-19 23:11:29 +0200

    TEST

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ const App = () => {
   const [userToken, setUserToken] = useState([]);
   const [groupNames, setGroupNames] = useState([]);
 
-  const loginRequest = { scopes: ["user_impersonation","email", "Group.Read.All", "GroupMember.Read.All", "openid", "profile", "User.Read", "User.Read.All", "User.ReadBasic.All"] };
+  const loginRequest = { scopes: ["user_impersonation", "email", "openid", "profile", "User.Read", "User.Read.All", "User.ReadBasic.All"] };
 
   useEffect(() => {
     const checkSession = async () => {
@@ -57,7 +57,7 @@ const App = () => {
         // Token acquisition
         try {
           const tokenResponse = await instance.acquireTokenSilent({
-            scopes: ["user_impersonation","email", "Group.Read.All", "GroupMember.Read.All", "openid", "profile", "User.Read", "User.Read.All", "User.ReadBasic.All"]
+            scopes: ["user_impersonation", "email", "openid", "profile", "User.Read", "User.Read.All", "User.ReadBasic.All"]
           });
           const token = tokenResponse.accessToken;
   
@@ -99,7 +99,7 @@ const App = () => {
       const email = loginResponse.account.username.split('@')[0];
       setUserName(loginResponse.account.name || loginResponse.account.username);
 
-      const token = (await instance.acquireTokenSilent({ scopes: ["user_impersonation","email", "Group.Read.All", "GroupMember.Read.All", "openid", "profile", "User.Read", "User.Read.All", "User.ReadBasic.All"] })).accessToken;
+      const token = (await instance.acquireTokenSilent({ scopes: ["user_impersonation", "email", "openid", "profile", "User.Read", "User.Read.All", "User.ReadBasic.All"] })).accessToken;
       console.log("App.js: Token acquired post-login.");
 
       const apiUrl = process.env.REACT_APP_API_GETAWAY_URL;
