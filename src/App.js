c00478115e00e7af15a258a499dce93f0b4545c4
wpeladmaster <drore@elad.co.il>
2024-12-22 12:24:43 +0200

    TEST scope

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ const App = () => {
   const [userToken, setUserToken] = useState([]);
   const [groupNames, setGroupNames] = useState([]);
 
-  const loginRequest = { scopes: ["openid", "profile", "email", "User.Read", "api://saml_barilan/user_impersonation"] };
+  const loginRequest = { scopes: ["openid", "profile", "email", "User.Read", "api://saml_barilan/user_impersonation/user_impersonation"] };
 
   useEffect(() => {
     const checkSession = async () => {
@@ -57,7 +57,7 @@ const App = () => {
         // Token acquisition
         try {
           const tokenResponse = await instance.acquireTokenSilent({
-            scopes: ["openid", "profile", "email", "User.Read", "api://saml_barilan/user_impersonation"]
+            scopes: ["openid", "profile", "email", "User.Read", "api://saml_barilan/user_impersonation/user_impersonation"]
           });
           const token = tokenResponse.accessToken;
   
@@ -99,7 +99,7 @@ const App = () => {
       const email = loginResponse.account.username.split('@')[0];
       setUserName(loginResponse.account.name || loginResponse.account.username);
 
-      const token = (await instance.acquireTokenSilent({ scopes: ["openid", "profile", "email", "User.Read", "api://saml_barilan/user_impersonation"] })).accessToken;
+      const token = (await instance.acquireTokenSilent({ scopes: ["openid", "profile", "email", "User.Read", "api://saml_barilan/user_impersonation/user_impersonation"] })).accessToken;
       console.log("App.js: Token acquired post-login.");
 
       const apiUrl = process.env.REACT_APP_API_GETAWAY_URL;
