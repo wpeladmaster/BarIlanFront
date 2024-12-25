a388555f35f3c5b25839e2587553028edb89c9f0
wpeladmaster <drore@elad.co.il>
2024-12-25 16:18:35 +0200

    Seprate stage and prod main url

--- src/index.js
+++ src/index.js
@@ -8,7 +8,7 @@ const msalConfig = {
   auth: {
     clientId: "aadb3f2f-d35f-4080-bc72-2ee32b741120",
     authority: "https://login.microsoftonline.com/352ed1fa-2f18-487f-a4cf-4804faa235c7/saml2",
-    redirectUri: "https://barilanclinics.eladtest.com/homepage",
+    redirectUri: process.env.REACT_APP_REDIRECT_URI
   },
   cache: {
     cacheLocation: "sessionStorage",
