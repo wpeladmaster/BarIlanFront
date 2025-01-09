77ef8c556ee4f87de5494034d59dd5f7100e1119
Dror Efraim <139956959+wpeladmaster@users.noreply.github.com>
2025-01-09 11:19:08 +0200

    Change footer size

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -6,9 +6,6 @@ const Footer = () => {
     <footer className="footer">
         <div className='footer-bg'></div>
         <div className='inner'>
-            <div className='logo'>
-                <img src='../../images/bar-ilan-logo.png' alt='Logo' />
-            </div>
             <p>&copy; {(new Date().getFullYear())} Bar Ilan University. All rights reserved.</p>
         </div>
     </footer>
